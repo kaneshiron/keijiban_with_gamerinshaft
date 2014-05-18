bf4a6f56063bd93a9c25e678fe08fc14c8d754cf
kaneshiron <maphmould@gmail.com>
2014-05-18 19:10:39 +0900

    最初からindexを表示するようにした（public/index.htmlの削除）

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ Testkeijiban::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+ root :to => 'boards#index'
 
   # See how all your routes lay out with "rake routes"
 
