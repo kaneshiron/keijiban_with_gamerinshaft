f1ddd788a841b798ee27a46085e3e40a2f27698e
kaneshiron <maphmould@gmail.com>
2014-05-18 19:05:24 +0900

    Commentがエラーの時の対処

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,12 @@
 class CommentsController < ApplicationController
   def update
+    @board = Board.find(params[:board_id])
+    @comment = Comment.find(params[:id])
+    if @comment.update_attributes(params[:comment])
+      redirect_to board_path(@board)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -18,11 +25,15 @@ class CommentsController < ApplicationController
 
   def index
   end
+
   def create
     @board = Board.find(params[:board_id])
     @comment = @board.comments.build(params[:comment])
-    @comment.save
-    redirect_to board_path(@board)
+    if  @comment.save
+      redirect_to board_path(@board)
+    else
+      render :new
+    end
   end
 
   def edit
