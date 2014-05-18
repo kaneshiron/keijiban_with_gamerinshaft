0aa58fab3fa7df8a6ba98290aab744c642b253d7
kaneshiron <maphmould@gmail.com>
2014-05-18 16:13:00 +0900

    validationの設定

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,5 @@
 class Board < ActiveRecord::Base
   attr_accessible :name, :content
+  validates :name, presence: true, length: {in: 4..9}, uniqueness: true
+  validates :content, presence: true
 end
