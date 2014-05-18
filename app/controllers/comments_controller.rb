ce3953393b7adf6d7930bd653766555f1d645449
kaneshiron <maphmould@gmail.com>
2014-05-18 18:23:03 +0900

    edit comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,14 +3,23 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @board = Board.find(params[:board_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to board_path(@board)
   end
 
   def new
+
   end
 
+  def show
+  end
+
+  def index
+  end
   def create
     @board = Board.find(params[:board_id])
-
     @comment = @board.comments.build(params[:comment])
     @comment.save
     redirect_to board_path(@board)
