3060941caa4c87a7affdd356c3fbe81208e67f89
kaneshiron <maphmould@gmail.com>
2014-05-18 12:56:03 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testkeijiban::Application.config.secret_token = '30fcc7edfa7ccd1871c5ba2cc16a529a9632bd85090499484b138cd11d5fd91f715fe4fbe785dac3396824982791fa861d377443180c9b1ec4bdd215230e8f25'
